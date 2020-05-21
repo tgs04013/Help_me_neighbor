1f90b675bb2c1c26d536bd22e73544f2465b12f0
LJBang <kailo123457@gmail.com>
2020-05-21 10:44:55 +0900

    signup

--- mypage/views.py
+++ mypage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 from django.shortcuts import render, redirect
@@ -6,3 +7,5 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def mypage_detail(request):
     return render(request, 'mypage/mypage_detail.html')
+
+
